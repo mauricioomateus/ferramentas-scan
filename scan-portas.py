d55db38a9ac98292b207d5bc77533e32056e2ed0
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2023-09-05 15:53:16 -0300

    Update scan-portas.py

--- scan-portas.py
+++ scan-portas.py
@@ -3,7 +3,7 @@ import socket, sys
 if len(sys.argv) > 1:
     print("[+] Varrendo o host: ")
     ip = sys.argv[1]
-    print("Inciando o Scan no...")
+    print("Inciando o Scan no Host {}...".format(ip))
     for porta in range(1,65535):
         meuSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if meuSocket.connect_ex((ip,porta)) == 0:
