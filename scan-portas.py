6ea043cd8a51552bb54a1ad96a680373a2c9a34c
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2023-09-05 15:52:16 -0300

    Create scan-portas.py

---
+++ scan-portas.py
@@ -0,0 +1,14 @@
+import socket, sys
+
+if len(sys.argv) > 1:
+    print("[+] Varrendo o host: ")
+    ip = sys.argv[1]
+    print("Inciando o Scan no...")
+    for porta in range(1,65535):
+        meuSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if meuSocket.connect_ex((ip,porta)) == 0:
+            print("Porta: " + str(porta) + " [ABERTA]")
+            meuSocket.close()
+else:
+    print("[+] Informe o Ip para realizar o scan [+]")
+    exit(0)
