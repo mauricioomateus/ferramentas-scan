a09424b06b0b58f74d81bbbfed2c1b451347bdd4
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2023-09-05 13:10:39 -0300

    Create scan_interno.py

---
+++ scan_interno.py
@@ -0,0 +1,32 @@
+# pip3 install scapy
+# download winpcap
+
+import scapy.all as scapy
+import socket
+
+#pegando uma requisição ARP
+request = scapy.ARP()
+
+request.pdst = '192.168.1.1/24'
+
+# mandar um broadcast para a red inteira
+broadcast = scapy.Ether() 
+
+# endereço de destino
+broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+
+request_broadcast = broadcast / request
+
+# pegará o endereço de ip
+clients = scapy.srp(request_broadcast, timeout=10, verbose=1)[0]
+
+for element in clients:
+    ip = element[1].psrc
+    mac = element[1].hwsrc
+    print(ip + "  :  " + mac)
+
+try:
+    a = socket.gethostbyaddr(ip)
+    print("hostname: " + a[0])
+except:
+    print("Hostname: DESCONHECIDO")
