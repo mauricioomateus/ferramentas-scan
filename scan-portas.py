bdaf3b302badd0ead6ff2ad24c0f1aaad379b6d8
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2023-09-06 13:20:22 -0300

    Update scan-portas.py

--- scan-portas.py
+++ scan-portas.py
@@ -1,14 +1,22 @@
-import socket, sys
+import socket, sys, threading
 
-if len(sys.argv) > 1:
-    print("[+] Varrendo o host: ")
-    ip = sys.argv[1]
-    print("Inciando o Scan no Host {}...".format(ip))
-    for porta in range(1,65535):
-        meuSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if meuSocket.connect_ex((ip,porta)) == 0:
+
+def portscan(porta):
+    meuSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    meusocket.settimeout(0.5)
+     if meuSocket.connect_ex((ip,porta)) == 0:
             print("Porta: " + str(porta) + " [ABERTA]")
             meuSocket.close()
+
+r = i #variavel para interação
+
+if len(sys.argv) > 1:
+    ip = sys.argv[1]    
+    print("[+] Varrendo o host: {}".format(ip))
+    for porta in range(1,65535):
+        t = threading.Thread(target=portscan, kwargs={'porta':r})
+        r += 1
+        t.start()
 else:
     print("[+] Informe o Ip para realizar o scan [+]")
     exit(0)
