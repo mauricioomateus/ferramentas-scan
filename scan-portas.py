1b87413e4398d7edd4b27955fab9c75865ba7132
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2023-09-06 15:46:41 -0300

    Update scan-portas.py

--- scan-portas.py
+++ scan-portas.py
@@ -2,8 +2,8 @@ import socket, sys, threading
 
 
 def portscan(porta):
-    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    meusocket.settimeout(0.5)
+    meuSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    meuSocket.settimeout(0.5)
      if meuSocket.connect_ex((ip,porta)) == 0:
             print("Porta: " + str(porta) + " [ABERTA]")
             meuSocket.close()
